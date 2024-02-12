aeef6d10a16ba195d1c2aa6989e39a1d39ab72bd
Pierre Plourde <pierre@spartancsharp.net>
2024-02-12 14:59:59 -0500

    Code Cleanup
    Clear Errors, Warnings, and Messages; run Code Cleanup Profile

--- SayHi/Models/ErrorViewModel.cs
+++ SayHi/Models/ErrorViewModel.cs
@@ -1,11 +1,15 @@
-using System;
-
 namespace SayHi.Models
 {
 	public class ErrorViewModel
 	{
 		public string RequestId { get; set; }
 
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+		public bool ShowRequestId
+		{
+			get
+			{
+				return !string.IsNullOrEmpty(RequestId);
+			}
+		}
 	}
 }
--- SayHi/Startup.cs
+++ SayHi/Startup.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +18,7 @@ namespace SayHi
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllersWithViews();
+			_ = services.AddControllersWithViews();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,27 +26,25 @@ namespace SayHi
 		{
 			if ( env.IsDevelopment() )
 			{
-				app.UseDeveloperExceptionPage();
+				_ = app.UseDeveloperExceptionPage();
 			}
 			else
 			{
-				app.UseExceptionHandler("/Home/Error");
+				_ = app.UseExceptionHandler("/Home/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
+				_ = app.UseHsts();
 			}
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
 
-			app.UseRouting();
+			_ = app.UseHttpsRedirection();
+			_ = app.UseStaticFiles();
 
-			app.UseAuthorization();
+			_ = app.UseRouting();
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllerRoute(
+			_ = app.UseAuthorization();
+
+			_ = app.UseEndpoints(endpoints => endpoints.MapControllerRoute(
 					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
-			});
+					pattern: "{controller=Home}/{action=Index}/{id?}"));
 		}
 	}
 }
